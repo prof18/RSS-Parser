559fb8ce6a87a8df41f2e206a8f6935ff86cf106
Marco Gomiero <marco.gomiero.93@gmail.com>
2018-05-18 19:07:52 +0200

    Changed behaviour to find img inside the description and the content

--- rssparser/src/main/java/com/prof/rssparser/XMLParser.java
+++ rssparser/src/main/java/com/prof/rssparser/XMLParser.java
@@ -17,10 +17,6 @@
 
 package com.prof.rssparser;
 
-import android.util.Log;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -30,6 +26,8 @@ import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Observable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Marco Gomiero on 12/02/2015.
@@ -59,82 +57,58 @@ public class XMLParser extends Observable {
         while (eventType != XmlPullParser.END_DOCUMENT) {
 
             if (eventType == XmlPullParser.START_TAG) {
-
                 if (xmlPullParser.getName().equalsIgnoreCase("item")) {
-
                     insideItem = true;
 
                 } else if (xmlPullParser.getName().equalsIgnoreCase("title")) {
-
                     if (insideItem) {
                         String title = xmlPullParser.nextText();
                         currentArticle.setTitle(title);
                     }
 
                 } else if (xmlPullParser.getName().equalsIgnoreCase("link")) {
-
                     if (insideItem) {
                         String link = xmlPullParser.nextText();
                         currentArticle.setLink(link);
                     }
 
                 } else if (xmlPullParser.getName().equalsIgnoreCase("dc:creator")) {
-
                     if (insideItem) {
                         String author = xmlPullParser.nextText();
                         currentArticle.setAuthor(author);
                     }
 
                 } else if (xmlPullParser.getName().equalsIgnoreCase("category")) {
-
                     if (insideItem) {
                         String category = xmlPullParser.nextText();
                         currentArticle.addCategory(category);
                     }
 
                 } else if (xmlPullParser.getName().equalsIgnoreCase("media:thumbnail")) {
-
                     if (insideItem) {
                         String img = xmlPullParser.getAttributeValue(null, "url");
                         currentArticle.setImage(img);
                     }
 
                 } else if (xmlPullParser.getName().equalsIgnoreCase("description")) {
-
                     if (insideItem) {
                         String description = xmlPullParser.nextText();
                         if (currentArticle.getImage() == null) {
-                            Document doc = Jsoup.parse(description);
-                            try {
-                                //choose the first image found in the description
-                                String pic = doc.select("img").first().attr("abs:src");
-                                currentArticle.setImage(pic);
-                            } catch (NullPointerException e) {
-                                currentArticle.setImage(null);
-                            }
+                            currentArticle.setImage(getImageUrl(description));
                         }
                         currentArticle.setDescription(description);
                     }
 
                 } else if (xmlPullParser.getName().equalsIgnoreCase("content:encoded")) {
-
                     if (insideItem) {
-                        String htmlData = xmlPullParser.nextText();
+                        String content = xmlPullParser.nextText();
                         if (currentArticle.getImage() == null) {
-                            Document doc = Jsoup.parse(htmlData);
-                            try {
-                                //choose the first image found in the article
-                                String pic = doc.select("img").first().attr("abs:src");
-                                currentArticle.setImage(pic);
-                            } catch (NullPointerException e) {
-                                currentArticle.setImage(null);
-                            }
+                            currentArticle.setImage(getImageUrl(content));
                         }
-                        currentArticle.setContent(htmlData);
+                        currentArticle.setContent(content);
                     }
 
                 } else if (xmlPullParser.getName().equalsIgnoreCase("pubDate")) {
-                    @SuppressWarnings("deprecation")
                     Date pubDate = new Date(xmlPullParser.nextText());
                     currentArticle.setPubDate(pubDate);
                 }
@@ -153,5 +127,27 @@ public class XMLParser extends Observable {
     private void triggerObserver() {
         setChanged();
         notifyObservers(articles);
+    }
+
+    /**
+     * Finds the first img tag and get the src as featured image
+     *
+     * @param input The content in which to search for the tag
+     * @return The url, if there is one
+     */
+    private String getImageUrl(String input) {
+
+        String url = null;
+        Pattern patternImg = Pattern.compile("(<img .*?>)");
+        Matcher matcherImg = patternImg.matcher(input);
+        if (matcherImg.find()) {
+            String imgTag = matcherImg.group(1);
+            Pattern patternLink = Pattern.compile("src\\s*=\\s*\"(.+?)\"");
+            Matcher matcherLink = patternLink.matcher(imgTag);
+            if (matcherLink.find()) {
+                url = matcherLink.group(1);
+            }
+        }
+        return url;
     }
 }
