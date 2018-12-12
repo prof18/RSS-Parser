d35b1e8dab7b30b9bcdd3d8be0d06e0869081901
Marco Gomiero <marco.gomiero.93@gmail.com>
2018-12-12 22:30:54 +0100

    Split kotlin and java sample app

---
+++ samplejava/src/main/java/com/prof/rssparser/sample/java/MainActivity.java
@@ -0,0 +1,185 @@
+package com.prof.rssparser.sample.java;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.prof.rssparser.Article;
+import com.prof.rssparser.Parser;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private ArticleAdapter mAdapter;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private ProgressBar progressBar;
+    private String urlString = "https://www.androidauthority.com/feed";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        progressBar = findViewById(R.id.progressBar);
+
+        mRecyclerView = findViewById(R.id.list);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setHasFixedSize(true);
+
+        mSwipeRefreshLayout = findViewById(R.id.container);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary, R.color.colorPrimaryDark);
+        mSwipeRefreshLayout.canChildScrollUp();
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+            @Override
+            public void onRefresh() {
+
+                mAdapter.clearData();
+                mAdapter.notifyDataSetChanged();
+                mSwipeRefreshLayout.setRefreshing(true);
+                loadFeed();
+            }
+        });
+
+        if (!isNetworkAvailable()) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(R.string.alert_message)
+                    .setTitle(R.string.alert_title)
+                    .setCancelable(false)
+                    .setPositiveButton(R.string.alert_positive,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog,
+                                                    int id) {
+                                    finish();
+                                }
+                            });
+
+            AlertDialog alert = builder.create();
+            alert.show();
+
+        } else if (isNetworkAvailable()) {
+            loadFeed();
+        }
+    }
+
+    public void loadFeed() {
+
+        if (!mSwipeRefreshLayout.isRefreshing())
+            progressBar.setVisibility(View.VISIBLE);
+
+        Parser parser = new Parser();
+        parser.execute(urlString);
+        parser.onFinish(new Parser.OnTaskCompleted() {
+            //what to do when the parsing is done
+            @Override
+            public void onTaskCompleted(ArrayList<Article> list) {
+                //list is an Array List with all article's information
+                //set the adapter to recycler view
+                mAdapter = new ArticleAdapter(list, R.layout.row, MainActivity.this);
+                mRecyclerView.setAdapter(mAdapter);
+                progressBar.setVisibility(View.GONE);
+                mSwipeRefreshLayout.setRefreshing(false);
+
+            }
+
+            //what to do in case of error
+            @Override
+            public void onError(Exception exception) {
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.GONE);
+                        mSwipeRefreshLayout.setRefreshing(false);
+                        Toast.makeText(MainActivity.this, "Unable to load data.",
+                                Toast.LENGTH_LONG).show();
+                        Log.i("Unable to load ", "articles");
+                    }
+                });
+            }
+        });
+    }
+
+    public boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    @Override
+    public void onResume() {
+
+        super.onResume();
+        if (mAdapter != null)
+            mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+        if (mAdapter != null)
+            mAdapter.clearData();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            android.support.v7.app.AlertDialog alertDialog = new android.support.v7.app.AlertDialog.Builder(MainActivity.this).create();
+            alertDialog.setTitle(R.string.app_name);
+            alertDialog.setMessage(Html.fromHtml(MainActivity.this.getString(R.string.info_text) +
+                    " <a href='http://github.com/prof18/RSS-Parser'>GitHub.</a>" +
+                    MainActivity.this.getString(R.string.author)));
+            alertDialog.setButton(android.support.v7.app.AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+
+            ((TextView) alertDialog.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+
