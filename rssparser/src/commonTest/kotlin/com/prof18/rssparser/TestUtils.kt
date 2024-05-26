00b6de37a647d6eb8a20e81592cfd112c4ceb852
Marco Gomiero <marco.gomiero.93@gmail.com>
2024-05-26 09:37:37 +0200

    Add klib validator. Set source and target jvm back to 8

--- rssparser/src/commonTest/kotlin/com/prof18/rssparser/TestUtils.kt
+++ rssparser/src/commonTest/kotlin/com/prof18/rssparser/TestUtils.kt
@@ -2,7 +2,6 @@ package com.prof18.rssparser
 
 import com.prof18.rssparser.internal.ParserInput
 
-@Suppress("NO_ACTUAL_FOR_EXPECT")
 internal expect fun readFileFromResources(
     resourceName: String
 ): ParserInput
@@ -11,5 +10,4 @@ internal expect fun readFileFromResourcesAsString(
     resourceName: String
 ): String
 
-@Suppress("NO_ACTUAL_FOR_EXPECT")
 internal expect val currentTarget: CurrentTarget
