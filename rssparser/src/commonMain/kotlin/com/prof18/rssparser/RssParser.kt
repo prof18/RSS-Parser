401fd149d8e88cfd59c39b4778ef0b1338d9cd49
Marco Gomiero <marco.gomiero.93@gmail.com>
2025-02-27 20:51:59 +0100

    Try to fix malformed feeds

--- rssparser/src/commonMain/kotlin/com/prof18/rssparser/RssParser.kt
+++ rssparser/src/commonMain/kotlin/com/prof18/rssparser/RssParser.kt
@@ -28,7 +28,8 @@ public class RssParser internal constructor(
      * Downloads and parses an RSS feed from an [url] and returns an [RssChannel].
      *
      * If the parsing fails because the XML is malformed, it will re-download the XML as a string,
-     * clean it up and try to parse it again. If it fails again, it will throw an [RssParsingException].
+     * escape any invalid XML entities, and try to parse it again. If it still fails, it will
+     * throw an [RssParsingException].
      */
     public suspend fun getRssChannel(url: String): RssChannel = withContext(coroutineContext) {
         val parserInput = xmlFetcher.fetchXml(url)
@@ -36,8 +37,9 @@ public class RssParser internal constructor(
             xmlParser.parseXML(parserInput)
         } catch (_: RssParsingException) {
             val xmlAsString = xmlFetcher.fetchXmlAsString(url)
-            val input = xmlParser.generateParserInputFromString(xmlAsString)
-            xmlParser.parseXML(input)
+            val escapedXml = escapeInvalidXmlEntities(xmlAsString)
+            val escapedInput = xmlParser.generateParserInputFromString(escapedXml)
+            xmlParser.parseXML(escapedInput)
         }
     }
 
@@ -46,7 +48,46 @@ public class RssParser internal constructor(
      */
     public suspend fun parse(rawRssFeed: String): RssChannel = withContext(coroutineContext) {
         val parserInput = xmlParser.generateParserInputFromString(rawRssFeed)
-        return@withContext xmlParser.parseXML(parserInput)
+        return@withContext try {
+            xmlParser.parseXML(parserInput)
+        } catch (_: RssParsingException) {
+            // Try again with additional entity escaping
+            val escapedXml = escapeInvalidXmlEntities(rawRssFeed)
+            val input = xmlParser.generateParserInputFromString(escapedXml)
+            xmlParser.parseXML(input)
+        }
+    }
+
+    /**
+     * Escapes invalid XML entities in the input string and fixes common XML issues.
+     * - Escapes standalone ampersands that aren't part of valid entity references
+     * - Fixes self-closing or unclosed tags that should be properly closed
+     * - Fixes duplicate closing tags with content between them
+     */
+    private fun escapeInvalidXmlEntities(xml: String): String {
+        return xml
+            // Fix standalone ampersands
+            .replace(
+                Regex("&(?!(amp;|lt;|gt;|quot;|apos;|#[0-9]+;|#x[0-9a-fA-F]+;))"),
+                "&amp;"
+            )
+            // Fix duplicate closing tags with content between them
+            // Example: <category></category><![CDATA[News]]></category> -> <category><![CDATA[News]]></category>
+            .replace(
+                Regex("<([^>]+)></\\1>([^<]+|<!\\[CDATA\\[.+?\\]\\]>)</\\1>"),
+                "<$1>$2</$1>"
+            )
+            // Fix self-closing tags, but only if they don't already have content
+            // This regex checks that there's no content between the opening and closing tags
+            .replace(
+                Regex("<(link|source|category|guid|enclosure|media:content|media:thumbnail)([^>]*?)>\\s*</\\1>"),
+                "<$1$2></$1>"
+            )
+            // Fix other common HTML tags that might be self-closing
+            .replace(
+                Regex("<(meta|img|br|hr|input|area|base|col|embed|keygen|param|track|wbr)([^>]*?)/?>(?!</\\1>)"),
+                "<$1$2></$1>"
+            )
     }
 }
 
