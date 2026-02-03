9f18d24818866af372aa76f759e2a85f5fba0e8e
Marco Gomiero <marco.gomiero.93@gmail.com>
2026-02-03 21:31:22 +0100

    Migrate to new AGP structure for Kotlin Multiplatform libraries

--- samples/multiplatform/shared/build.gradle.kts
+++ samples/multiplatform/shared/build.gradle.kts
@@ -2,16 +2,22 @@
 
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.multiplatform)
 }
 
 kotlin {
     jvmToolchain(17)
 
-    androidTarget()
+    androidLibrary {
+        namespace = "com.prof18.rssparser.sample"
+        compileSdk = libs.versions.android.compile.sdk.get().toInt()
+        minSdk = libs.versions.android.min.sdk.get().toInt()
+        compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
+    }
 
     jvm("desktop")
 
@@ -55,15 +61,3 @@ kotlin {
     }
 }
 
-android {
-    namespace = "com.prof18.rssparser.sample"
-    compileSdk = libs.versions.android.compile.sdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.android.min.sdk.get().toInt()
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-}
