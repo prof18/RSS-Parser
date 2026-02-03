4f976fded79b0cb496785b9fc6f0a7153334f765
Marco Gomiero <marco.gomiero.93@gmail.com>
2026-02-03 12:13:42 +0100

    Prefer Atom entry alternate links and ignore related media URLs

--- rssparser/src/jvmMain/kotlin/com/prof18/rssparser/internal/atom/AtomFeedHandler.kt
+++ rssparser/src/jvmMain/kotlin/com/prof18/rssparser/internal/atom/AtomFeedHandler.kt
@@ -53,7 +53,7 @@ internal class AtomFeedHandler(
                         href
                     }
                     when {
-                        isInsideItem -> channelFactory.articleBuilder.link(link)
+                        isInsideItem -> channelFactory.articleBuilder.link(link, rel)
                         else -> channelFactory.channelBuilder.link(link)
                     }
                 }
